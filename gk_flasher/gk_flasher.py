b5567042348ad91dd48cd7097c1af4c5beef6c58
alufers <alufers@wp.pl>
2023-07-06 21:58:36 +0200

    FIx pre-commit stuff

--- gk_flasher/gk_flasher.py
+++ gk_flasher/gk_flasher.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*-
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Simple to use flash utility for microcontrollers")
-    parser.add_argument("--package-esp32", help="Path to esp-idf's build/ directory to build a GK Flasher package from", ty)
-    args = parser.parse_args()
-    
+    parser = argparse.ArgumentParser(
+        description="Simple to use flash utility for microcontrollers"
+    )
+    parser.add_argument(
+        "--package-esp32",
+        help="Path to esp-idf's build/ directory to\
+             build a GK Flasher package from",
+        type=str,
+    )
+    parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
