c9f48479ef55581213027ba095d7184bfe16ed7f
alufers <alufers@wp.pl>
2023-07-06 21:45:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gk-flasher
+
+A GUI and command line utility to flash software on ESP32 devices using convenient packages.
---
+++ gk_flasher/gk_flasher.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple to use flash utility for microcontrollers")
+    parser.add_argument("--package-esp32", help="Path to esp-idf's build/ directory to build a GK Flasher package from", ty)
+    args = parser.parse_args()
+    
+
+if __name__ == "__main__":
+    main()
